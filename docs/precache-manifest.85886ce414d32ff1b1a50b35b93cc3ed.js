5f9bdcdf4a296cef31db8d94746fbf877c6fe3b7
patgaw09318 <patgaw09318@gmail.com>
2019-04-27 02:26:56 +0200

    Demo added

---
+++ docs/precache-manifest.85886ce414d32ff1b1a50b35b93cc3ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0bd8ec101f1d4dd8b23",
+    "url": "/static/css/main.2e1260d2.chunk.css"
+  },
+  {
+    "revision": "a0bd8ec101f1d4dd8b23",
+    "url": "/static/js/main.237eec18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "671d8646e61a07e86cc5",
+    "url": "/static/js/2.710c7aed.chunk.js"
+  },
+  {
+    "revision": "bd9880563443c0e1be2ad7ccac776fcd",
+    "url": "/index.html"
+  }
+];
